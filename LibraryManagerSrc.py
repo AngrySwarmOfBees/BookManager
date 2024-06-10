1d24a7de867800af124b8cc19fc1153997380b88
Bee <ben.mowery@logosprep.org>
2024-06-09 22:24:47 -0500

    Added scalable sidebar
    + cleaned up some variable names

--- LibraryManagerSrc.py
+++ LibraryManagerSrc.py
@@ -16,15 +16,17 @@ from PIL import Image, ImageTk
 from turtle import bgcolor, color, left, right, width
 
 
-#Variable Setup
+
 
 #globals
+#global MainCanvasItems = []
+global SideMenuPanel
 
 #nonglobals
 
 #--UI-----------------------------------
 #UI Setup
-Window = tk.Tk() #initialize window
+Root = Tk() #initialize window
 
 #UI Style Setup
 HeaderFont = TkFont.Font(family="SF Pro Display", size=16, weight="bold")     #Initialize Font standard for headers
@@ -34,26 +36,66 @@ DarkModeStyle = ttk.Style()
 DarkModeStyle.configure("Dark.Mode", foreground='#bb86fc')
 LightModeStyle = ttk.Style()
 LightModeStyle.configure("Light.Mode", foreground='#6200ee')
-Window.title("Library Manager")   #set window title   
-Window.geometry('960x540+50+50')    #set window default size
-Window.configure(bg="#121212")  #set background color(default dark mode)
-Window.resizable(False, False)
-Window.geometry('960x540+50+50')    #set window default size
-Window.configure(bg="#121212")  #set background color(default dark mode)
-Window.resizable(True, True)
+Root.title("Library Manager")   #set window title   
+Root.geometry('960x540+50+50')    #set window default size
+Root.configure(bg="#121212")  #set background color(default dark mode)
+Root.resizable(False, False)
+Root.geometry('960x540+50+50')    #set window default size
+Root.configure(bg="#121212")  #set background color(default dark mode)
+Root.resizable(True, True)
+
+#functions for buttons
+def PlaceholderFunc():
+    print("Button pressed")
 
 #Main window setup
-MainCanvas = tk.Canvas(Window, background="#1F1B24", height=960, width=270, bd="0", highlightthickness="0")
+MainCanvas = tk.Canvas(Root, background="#1F1B24", height=6, width=270, bd="0", highlightthickness="0")
 MainCanvas.place(x=0, y=0, relheight="1", relwidth="1")
-
 #placeholders
-PlaceHolderText = MainCanvas.create_text(0, 0, text="Placeholder!", fill="#bb86fc", font=HeaderFont)
+#PlaceHolderText = tk.Label(MainCanvas, text ="Placeholder", bg="#1F1B24", fg="#bb86fc", font=HeaderFont)
+#PlaceHolderText.grid(row= 0, column=0)
+
+SideMenuPanel=tk.Canvas(Root, background="#1f1f1f", height=6, width=50, bd="0", highlightthickness="0")     #Create left side bar
+SideMenuPanel.pack(side=LEFT)   #add left side bar
+SideBarMenuButton=tk.Button(SideMenuPanel, text="MB", command=PlaceholderFunc, bg="#1f1f1f", fg="#bb86fc", activebackground="#363636", activeforeground="#bb86fc", bd="0")  #Initialize Button
+SideBarMenuButton.place(x=0, y=0, relwidth="1", height=45) #Place Menu button
+SideBarFileButton = tk.Button(SideMenuPanel, text="FM", font=BodyFont, command=PlaceholderFunc, bg="#1f1f1f", fg="#bb86fc", activebackground="#363636", activeforeground="#bb86fc", bd="0")  #Initialize Button
+SideBarFileButton.place(x=0, y=45, relwidth="1", height=45)    #Add open file button
+SideBarDarkModeButton = tk.Button(SideMenuPanel, text="DM", font=BodyFont, command=PlaceholderFunc, bg="#1f1f1f", fg="#bb86fc", activebackground="#363636", activeforeground="#bb86fc", bd="0")  #Initialze button
+SideBarDarkModeButton.place(x=0, y=90, relwidth="1", height=45)    #Add toggle dark mode button
+SideBarSettingsButton = tk.Button(SideMenuPanel, text="SM", font=BodyFont, command=PlaceholderFunc, bg="#1f1f1f", fg="#bb86fc", activebackground="#363636", activeforeground="#bb86fc", bd="0")  #Initialize Button
+SideBarSettingsButton.place(x=0, y=480, relwidth="1", height=45)   #Place Settings Button
+
+class App(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self.parent = parent
+
+        # initialize the new_state
+        self.new_state = 'normal'
+
+        self.parent.bind('<Configure>', self._resize_handler)
+
+    def _resize_handler(self, event):
+        self.old_state = self.new_state # assign the old state value
+        self.new_state = self.parent.state() # get the new state value
+
+        if self.new_state == 'zoomed':
+            print('maximize event')
+        elif self.new_state == 'normal' and self.old_state == 'zoomed':
+            print('restore event')
+        else:
+            #print('dragged resize event')
+            #Update items
+            Root.update()
+            #Menu Bar---------------------------------------------------------------------------------------
+            SideMenuPanel.config(height=(Root.winfo_height()))
+            SideBarMenuButton.place(x=0, y=0, relwidth="1", height=45)
+            SideBarFileButton.place(x=0, y=(45), relwidth="1", height=45)    #update open file button
+            SideBarDarkModeButton.place(x=0, y=(90), relwidth="1", height=45)    #update toggle dark mode button
+            SideBarSettingsButton.place(x=0, y=(Root.winfo_height() - 45), relwidth="1", height=45)   #update Settings Button
 
 
-def UpdateWindow(newX, newY) {
-    #update item locations
-}
+App(Root).pack()
 
-
-
-Window.mainloop()
+Root.mainloop()
