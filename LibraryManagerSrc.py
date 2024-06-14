359992d81f3e6b555ec9d3384de0d49871e10548
Bee <ben.mowery@logosprep.org>
2024-06-13 22:22:11 -0500

    Fixxed border

--- LibraryManagerSrc.py
+++ LibraryManagerSrc.py
@@ -157,9 +157,9 @@ class ScrollableFrame(ttk.Frame):
     BodyWidgetStyle.configure('TFrame', background='#1F1B24')
     def __init__(self, container, *args, **kwargs):
         super().__init__(container, *args, **kwargs)
-        canvas = tk.Canvas(self, background="#1F1B24", bd=0)
+        canvas = tk.Canvas(self, background="#1F1B24", bd=0, borderwidth=0, border=0, highlightthickness="0")
         scrollbar = ttk.Scrollbar(Root, orient="vertical", command=canvas.yview)
-        self.scrollable_frame = ttk.Frame(canvas, style='TFrame')
+        self.scrollable_frame = ttk.Frame(canvas, style='TFrame', borderwidth=0, border=0)
 
         self.scrollable_frame.bind(
             "<Configure>",
@@ -170,7 +170,7 @@ class ScrollableFrame(ttk.Frame):
 
         canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
 
-        canvas.configure(yscrollcommand=scrollbar.set, background="#1F1B24")
+        canvas.configure(yscrollcommand=scrollbar.set, background="#1F1B24", borderwidth=0, border=0)
 
         canvas.pack(side="left", fill="both", expand=True)
         scrollbar.pack(side="right", fill="y")
